aebaee9f85a0909d86fd3ca88f527f2c07a54156
Sai Preetham Kommineni <skommineni05@gmail.com>
2025-11-11 23:52:34 -0500

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import SearchBar from "./searchbar";
 
-function App() {
+export default function App() {
+  const handleSearch = (q) => {
+    console.log("Searching for:", q);
+    // call your API here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center-wrapper" >
+      <SearchBar onSearch={handleSearch} />
     </div>
   );
 }
 
-export default App;
+
